7c7d62fb628c7b65ad87f2d64e9716d5f8ca85d9
moondiverc <nezzaluna10@gmail.com>
2025-09-10 03:45:03 +0700

    doc: change model and migrate

--- main/models.py
+++ main/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+
     name = models.CharField(max_length=255)
     price = models.IntegerField(default=0)
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    category = models.CharField(max_length=20, default='update')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
     
     def __str__(self):
