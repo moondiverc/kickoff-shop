a5fd729c614790936a9bf734297f6a2e02810254
moondiverc <nezzaluna10@gmail.com>
2025-09-15 13:53:09 +0700

    doc: change data model in models.py

--- main/models.py
+++ main/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='apparel')
     is_featured = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.name
