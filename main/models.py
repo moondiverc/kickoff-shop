324ca87e157c3dc8bfa6949f4213d9412ab36b1f
moondiverc <nezzaluna10@gmail.com>
2025-09-15 14:23:27 +0700

    doc: change data model in models.py

--- main/models.py
+++ main/models.py
@@ -14,11 +14,11 @@ class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
     description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='apparel')
     is_featured = models.BooleanField(default=False)
-    rating = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
