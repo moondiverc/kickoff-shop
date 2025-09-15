960432bd300ade5de18af5a8cb9f9490e4d5a87d
moondiverc <nezzaluna10@gmail.com>
2025-09-15 13:08:39 +0700

    doc: add data model in models.py

--- main/models.py
+++ main/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='apparel')
     is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
