bed046565d6e485817efb907b6d940068b8a7d01
moondiverc <nezzaluna10@gmail.com>
2025-09-29 17:31:50 +0700

    chore: finish tugas 5, add styling, edit & delete product

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_product, register, login_user, logout_user, edit_product, delete_product, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_product, show_product, register, login_user, logout_user, edit_product, delete_product, show_product_by_category, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
