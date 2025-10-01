611385da80495726e423822d3a2d125e21b6c3e2
moondiverc <nezzaluna10@gmail.com>
2025-10-01 08:12:16 +0700

    chore: finish tugas 5, add styling, edit & delete product

--- main/views.py
+++ main/views.py
@@ -72,20 +72,20 @@ def register(request):
 
 # fungsi untuk login user
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+    if form.is_valid():
         user = form.get_user()
         login(request, user)
         response = HttpResponseRedirect(reverse("main:show_main"))
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
 
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 # fungsi untuk logout user
 def logout_user(request):
@@ -136,18 +136,18 @@ def show_json(request):
 
 # mengembalikan data dalam bentuk XML berdasarkan ID
 def show_xml_by_id(request, product_id):
-   try:
-       product_item = Product.objects.filter(pk=product_id)
-       xml_data = serializers.serialize("xml", product_item)
-       return HttpResponse(xml_data, content_type="application/xml")
-   except Product.DoesNotExist:
-       return HttpResponse(status=404)
+    try:
+        product_item = Product.objects.filter(pk=product_id)
+        xml_data = serializers.serialize("xml", product_item)
+        return HttpResponse(xml_data, content_type="application/xml")
+    except Product.DoesNotExist:
+        return HttpResponse(status=404)
 
 # mengembalikan data dalam bentuk JSON berdasarkan ID
 def show_json_by_id(request, product_id):
-   try:
-       product_item = Product.objects.get(pk=product_id)
-       json_data = serializers.serialize("json", [product_item])
-       return HttpResponse(json_data, content_type="application/json")
-   except Product.DoesNotExist:
-       return HttpResponse(status=404)
+    try:
+        product_item = Product.objects.get(pk=product_id)
+        json_data = serializers.serialize("json", [product_item])
+        return HttpResponse(json_data, content_type="application/json")
+    except Product.DoesNotExist:
+        return HttpResponse(status=404)
