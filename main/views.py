7acf46937474409f0f954f9f5d3962861fdec35d
moondiverc <nezzaluna10@gmail.com>
2025-10-01 11:51:11 +0700

    chore: finish tugas 5, add styling, edit & delete product

--- main/views.py
+++ main/views.py
@@ -74,16 +74,16 @@ def register(request):
 def login_user(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
-
-    if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
-
+        
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
     else:
         form = AuthenticationForm(request)
+    
     context = {'form': form}
     return render(request, 'login.html', context)
 
